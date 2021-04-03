2029085cbdaa1497ec1d0764f25ccbae40087245
Ankit Batra <ankit.batra@tivo.com>
2021-04-04 02:15:57 +0530

    removed

--- app/src/main/java/com/abatra/android/allowance/demo/Utils.java
+++ app/src/main/java/com/abatra/android/allowance/demo/Utils.java
@@ -8,7 +8,7 @@ import com.abatra.android.allowance.consent.lib.ConsentLibConsentLoadRequest;
 
 public interface Utils {
 
-    String PUBLISHER_ID = "pub-4285683658805312";
+    String PUBLISHER_ID = "";
     String TEST_DEVICE = "98612BBF1433B7833F375AE392714233";
     String PRIVACY_POLICY = "http://www.app.com/privacyPolicy";
 
